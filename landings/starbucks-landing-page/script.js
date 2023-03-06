e08a8cf559981abdb53ee561c44a0c4c459a2e12
DimaChekashov <foxdima99@gmail.com>
2023-03-06 17:15:16 +0100

    implemented responsive for Starbucks Landing

--- landings/starbucks-landing-page/script.js
+++ landings/starbucks-landing-page/script.js
@@ -1,16 +1,11 @@
-function imgSlider(url) {
-    document.querySelector(".starbucks").src = url;
-}
-
-function changeCircleColor(color) {
-    const circle = document.querySelector(".circle");
-
-    circle.style.background = color;
-}
-
 document.querySelectorAll(".thumb-img").forEach((img) => {
     img.addEventListener("click", () => {
-        imgSlider(img.getAttribute("data-img"));
-        changeCircleColor(img.getAttribute("data-circle"));
+        document.querySelector(".starbucks").src = img.getAttribute("data-img");
+        document.querySelector(".circle").style.background = img.getAttribute("data-circle");
     });
+});
+
+document.querySelector(".toggle").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.querySelector(".navigation").classList.toggle("active");
 });
